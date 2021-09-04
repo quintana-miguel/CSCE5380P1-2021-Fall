b5c50d46ccf6ac575956e23bbcca15cc9dd5378e
Miguel Quintana-Hernandez <quintana.miguel@gmail.com>
2021-09-04 01:22:16 -0500

    ok

--- GPU test/test.py
+++ GPU test/test.py
@@ -1,1 +1,13 @@
-import tensorflow
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+import pandas as pd
+import seaborn as sns
+from pylab import rcParams
+import matplotlib.pyplot as plt
+from matplotlib import rc
+from pandas.plotting import register_matplotlib_converters
+print(tf.__version__)
+from sklearn.model_selection import train_test_split
+
+print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
